9b33afd6daa9c37a1babbaf1f1089bf17fb6a38c
Upigaiproject <upigaiproject@gmail.com>
2026-02-01 14:22:43 +0300

    Update api.ts

--- frontend/src/ config/api.ts
+++ frontend/src/ config/api.ts
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3002';
+export const API_URL = import.meta.env.VITE_API_URL || 'https://tenkap-app-production-up.railway.app';
